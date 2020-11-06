3b9b5ea9ce3d9887a2bd0d520a8c4fee1466a4bb
afterburner <rockey1024@icloud.com>
2020-11-06 15:57:21 +0800

    Released DistributedVisionRunner.App V1.0.0

--- DistributedVisionRunner.App/Views/MainWindow.xaml.cs
+++ DistributedVisionRunner.App/Views/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using Afterbunny.Windows.Helpers;
 using DistributedVisionRunner.Module;
+using Squirrel;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace DistributedVisionRunner.App.Views
@@ -15,11 +18,31 @@ namespace DistributedVisionRunner.App.Views
             InitializeComponent();
 
             this.SetMinimizeToTray("icon.ico");
+
+            Loaded += async (sender, args) =>
+            {
+                await CheckForUpdateAsync();
+            };
         }
 
         private void MainWindow_OnClosed(object sender, EventArgs e)
         {
             FileSystemHelper.RemoveOutdatedFiles(Constants.AppDataDir, TimeSpan.FromDays(30), "log");
+        }
+
+        private async Task CheckForUpdateAsync()
+        {
+            var parentDir = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+            var updateExe = Path.Combine(parentDir, "Update.exe");
+            var released = File.Exists(updateExe);
+            if (released)
+            {
+
+                using (var manager = new UpdateManager(KnownFolders.GetDefaultPath(KnownFolder.Downloads)))
+                {
+                    await manager.UpdateApp();
+                }
+            }
         }
 
     }
